196a9bdd2fdc9b9595cf84b432e8c2297c0ba99f
Yoander Valdés Rodríguez <yoander.valdes@gmail.com>
2014-12-03 19:09:41 -0500

    Add bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+define('TREE_ROOT_DIR', __DIR__);
+define('TREE_VENDOR_DIR', TREE_ROOT_DIR . '/vendor');
+
+require_once TREE_VENDOR_DIR . '/voltus-autoloader/src/Psr4Autoloader.php';
+
+// instantiate the loader
+$loader = new \Voltus\Autoloader\Psr4Autoloader();
+
+// register the autoloader
+$loader->register();
+
+// register the base directories for the namespace prefix
+$loader->addNamespace('Tree', TREE_ROOT_DIR . '/src/');
+$loader->addNamespace('Voltus\Pattern', __DIR__ . '/vendor/voltus-pattern/src');
+$loader->addNamespace('Voltus\Slugify', __DIR__ . '/vendor/voltus-slugify/src');
+$loader->addNamespace('Voltus\Persisten\File', __DIR__ . '/vendor/voltus-file/src');
